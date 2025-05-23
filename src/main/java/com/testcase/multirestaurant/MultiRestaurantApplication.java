ce51fce79ff57d5377194fd1972187a6608a6b97
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-05-23 21:40:54 +0300

    Initial commit

---
+++ src/main/java/com/testcase/multirestaurant/MultiRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.testcase.multirestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiRestaurantApplication.class, args);
+	}
+
+}
